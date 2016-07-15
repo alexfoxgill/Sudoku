ed5385880f41dd4d2a9042f61cf875ad86968ad5
alex.gill <alex.gill@penna.com>
2016-07-15 15:06:41 +0100

    Initial commit

---
+++ Solve.fs
@@ -0,0 +1,57 @@
+﻿module Solve
+
+open Structure
+
+module Action =
+    type Action = Elimination of Coordinate.Coordinate * Symbol
+
+    let apply a g =
+        match a with
+        | Elimination (co, s) ->
+            match Grid.get co g |> Cell.eliminate s with
+            | Some v -> Grid.set co v g
+            | _ -> g
+
+
+module Group =
+    let getActions (Group.Group g) =
+        let rec comb n l =
+            match (n,l) with
+            | (0,_) -> [[]]
+            | (_,[]) -> []
+            | (n,x::xs) ->
+                let useX = List.map (fun l -> x::l) (comb (n-1) xs)
+                let noX = comb n xs
+                useX @ noX
+
+        let actionForCells =
+            function
+            | [_, Cell.Certain elim; co, Cell.Possible xs]
+            | [co, Cell.Possible xs; _, Cell.Certain elim]
+                when Set.contains elim xs -> Action.Elimination (co, elim) |> Some
+            | _ -> None
+        
+        g
+        |> Map.toList
+        |> comb 2
+        |> Seq.choose actionForCells
+            
+
+module Grid =
+    let rec solve g =
+        let actions =
+            seq {
+                let min,max = bounds
+                for x in min..max do
+                for f in [Grid.getRow; Grid.getCol; Grid.getSqr] do
+                let group = f x g
+                match f x g with
+                | Some group -> yield! Group.getActions group
+                | None -> ()
+            } |> Seq.toList
+
+        if List.isEmpty actions then g
+        else
+            List.fold (fun g a -> Action.apply a g) g actions
+            |> solve
+        
---
+++ Structure.fs
@@ -0,0 +1,83 @@
+﻿module Structure
+
+open System
+
+let bounds = 0,8
+let (>=<) x (min, max) = min <= x && x <= max
+let clamp (x: int) (min, max) = Math.Max(Math.Min(x, max), min)
+
+type Symbol = One | Two | Three | Four | Five | Six | Seven | Eight | Nine
+    with
+    static member all = [| One; Two; Three; Four; Five; Six; Seven; Eight; Nine |]
+
+
+module Cell =
+    type Cell =
+        private
+        | Certain of Symbol
+        | Possible of Set<Symbol>
+
+    let init = set Symbol.all |> Possible
+    let certain = Certain
+
+    let create xs =
+        match Set.count xs with
+        | 0 -> None
+        | 1 -> xs |> Set.toSeq |> Seq.head |> Certain |> Some
+        | _ -> Possible xs |> Some
+
+    let eliminate x = function
+        | Possible xs -> Set.remove x xs |> create
+        | i -> i |> Some
+
+    let (|Certain|Possible|) = function
+    | Certain x -> Certain x
+    | Possible xs -> Possible xs
+
+    let isCertain = function
+    | Certain _ -> true
+    | _ -> false
+
+
+module Coordinate =
+    type Coordinate = private Coordinate of int * int
+    let (|Coordinate|) = function Coordinate (x,y) -> x,y
+    let zero = Coordinate (0,0)
+    let all =
+        let min,max = bounds
+        seq { for y in min..max do for x in min..max do yield Coordinate (x,y) }
+
+    let add (dx,dy) (Coordinate (x,y)) = (clamp (x+dx) bounds, clamp (y+dy) bounds) |> Coordinate
+    let create (x,y) = if x >=< bounds && y >=< bounds then Coordinate (x,y)|> Some else None
+    let row (Coordinate (x,y)) = x
+    let column (Coordinate (x,y)) = y
+    let square (Coordinate (x,y)) = (x/3)+(3*(y/3))
+    
+module Group =
+    type Group = Group of Map<Coordinate.Coordinate, Cell.Cell>
+
+module Grid =
+    type Grid = private Grid of Map<Coordinate.Coordinate, Cell.Cell>
+
+    let private apply f (Grid g) = g |> f |> Grid
+    let init =
+        Coordinate.all
+        |> Seq.map (fun co -> co, Cell.init)
+        |> Map.ofSeq
+        |> Grid
+
+    let get co (Grid g) = Map.find co g
+    let set co v g = apply (Map.add co v) g
+
+    let create xs = Seq.fold (fun g (co,v) -> set co v g) init
+
+    let private getGroup groupf n (Grid g) =
+        if (n >=< bounds)
+        then g |> Map.filter (fun co _ -> groupf co = n) |> Group.Group |> Some
+        else None
+
+    let getRow = getGroup Coordinate.row
+    let getCol = getGroup Coordinate.column
+    let getSqr = getGroup Coordinate.square
+
+    let isSolved (Grid g) = g |> Map.forall (fun _ c -> Cell.isCertain c)
